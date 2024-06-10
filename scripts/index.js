2afcfe5f47535e83f1c87e75bcb2730e2b20466a
Francisco Reyes <fjreyesq@gmail.com>
2024-06-09 23:45:25 -0700

    modal inputs

--- scripts/index.js
+++ scripts/index.js
@@ -28,13 +28,32 @@ const initialCards = [
 /* ------  Elements  ------- */
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
+const modalCloseButton = document.querySelector("#modal-close-button");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput= document.querySelector("#modal__input_title");
+const profileDescriptionInput= document.querySelector("#modal__input_description");
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+
+function closePopup() {
+    profileEditModal.classList.remove("modal_opened");
+}
+
 
 profileEditButton.addEventListener("click", () => {
-    profileEditModal.classList.add("modal_opened")
+    profileEditModal.classList.add("modal_opened");
+    profileTitleInput.value = profileTitle.textContent;
+    profileDescriptionInput.value = profileDescription.textContent;
 });
 
-const modalCloseButton = document.querySelector("#modal-close-button");
 
 modalCloseButton.addEventListener("click", () => {
-    profileEditModal.classList.remove("modal_opened")
+    closePopup();
+});
+
+profileEditForm.addEventListener("submit", () => {
+    e.preventDefault();
+    profileTitle.textContent = profileTitleInput.value;
+    profileDescription.textContent = profileDescriptionInput.value;
+    closePopup();
 });

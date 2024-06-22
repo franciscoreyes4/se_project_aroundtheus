45e67722551eefcbcd1e00a7007ef478f8590a3a
Francisco Reyes <fjreyesq@gmail.com>
2024-06-22 13:37:39 -0700

    s5 part 1 & 2

--- scripts/index.js
+++ scripts/index.js
@@ -28,18 +28,27 @@ const initialCards = [
 // Elements
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
-const modalCloseButton = document.querySelector("#modal-close-button");
+const addCardModal = document.querySelector("#add-card-modal");
+const addModalCloseButton = addCardModal.querySelector("#modal-close-button");
+const profileModalCloseButton = profileEditModal.querySelector("#modal-close-button");
 const profileTitle = document.querySelector(".profile__title");
 const profileDescription = document.querySelector(".profile__description");
-const profileTitleInput= document.querySelector("#modal__input_title");
+const profileTitleInput= document.querySelector("#modal__input_name");
 const profileDescriptionInput= document.querySelector("#modal__input_description");
 const profileEditForm = profileEditModal.querySelector(".modal__form");
 const cardList = document.querySelector(".cards__list");
 const cardTemplate = document.querySelector("#card-template").content.firstElementChild;
+const addNewCardButon = document.querySelector(".profile__add-button");
+const nameInput = profileEditModal.querySelector(".modal__input_type_name");
+const jobInput = profileEditModal.querySelector(".modal__input_type_description");
 
 // Functions
-function closePopup() {
-    profileEditModal.classList.remove("modal_opened");
+function closeModal(modal) {
+    modal.classList.remove("modal_opened");
+}
+
+function openModal(modal) {
+    modal.classList.add("modal_opened");
 }
 
 function getCardElement(cardData) {
@@ -57,20 +66,18 @@ function handleProfileEditSubmit(e) {
     e.preventDefault();
     profileTitle.textContent = profileTitleInput.value;
     profileDescription.textContent = profileDescriptionInput.value;
-    closePopup();
+    closeMoal();
 }
 
-// Event Listeners
-profileEditButton.addEventListener("click", () => {
-    profileEditModal.classList.add("modal_opened");
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+profileEditButton.addEventListener("click",() => {
+    openModal(profileEditModal);
     profileTitleInput.value = profileTitle.textContent;
     profileDescriptionInput.value = profileDescription.textContent;
 });
-
-
-modalCloseButton.addEventListener("click", closePopup);
-
-profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+profileModalCloseButton.addEventListener("click", () => closeModal(profileEditModal));
+addNewCardButon.addEventListener("click", () => openModal(addCardModal));
+addModalCloseButton.addEventListener("click", () => closeModal(addCardModal));
 
 initialCards.forEach((cardData) => {
     cardList.prepend(getCardElement(cardData));
